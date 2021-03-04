68c3b97936c08dfd43318000782e54102b6287bb
GoannaCodes <78517290+GoannaCodes@users.noreply.github.com>
2021-03-04 22:07:02 +1000

    Create forLoop.js

---
+++ Intro/forLoop.js
@@ -0,0 +1,11 @@
+/* for (tell node we want a for loop)
+   let i = 0 ( iterative variable)
+   i <=0 (if true, execute the code inside loop)
+    console.log("Hello World")
+  i++ increments the value of the iterative variable
+*/
+//prints out Hello World 10 times
+for (let i = 0; i <= 9; i++){
+  console.log("Hello World");
+}
+console.log("Finished"); //indicate end of loop, otherwise would print undefined if not included
