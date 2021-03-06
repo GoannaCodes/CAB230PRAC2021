28f27e7070f872d27becc2c064625923592bf83f
GoannaCodes <78517290+GoannaCodes@users.noreply.github.com>
2021-03-06 20:12:13 +1000

    Create undefined.js

---
+++ BasicTypes/UndefinedNull/undefined.js
@@ -0,0 +1,6 @@
+let nothing //not assigned variable
+console.log()
+console.log("Undefined vs Null")
+console.log(nothing)//displays as undefined as there is no value assigned to 'nothing' variable
+nothing = null //explictly written/returned
+console.log(nothing) //displays as null
